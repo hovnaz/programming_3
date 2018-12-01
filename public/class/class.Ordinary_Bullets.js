c0f6f2e2bf248b77577a62b858bf0a3e3f9532df
Your Name <you@example.com>
2018-12-01 16:54:03 +0400

    all fills arandznacreci class neri das 2

---
+++ public/class/class.Ordinary_Bullets.js
@@ -0,0 +1,97 @@
+class Ordinary_Bullets {
+	constructor(x, y, index, route) {
+		this.x = x;
+		this.y = y;
+		this.index = index;
+		this.route = route
+		this.multiply = 0;
+	}
+
+	newDirections() {
+		this.directions = [
+
+			[this.x, this.y - 1],
+
+			[this.x - 1, this.y],
+			[this.x + 1, this.y],
+
+			[this.x, this.y + 1],
+
+		];
+	}
+	chooseCell(character_move) {
+		this.newDirections()
+		var found = [];
+		var x = this.directions[character_move][0];
+		var y = this.directions[character_move][1];
+
+		if (x >= 0 && x < matrix.length && y >= 0 && y < matrix.length) {
+			if (matrix[y][x] == 0 || matrix[y][x] == 1) {
+				found.push(this.directions[character_move]);
+			}
+			// spanel 1 -n
+			if (matrix[y][x] == 3) {
+				matrix[y][x] = 0;
+				matrix[this.y][this.x] = 0;
+
+				for (var i in wolfArr) {
+					wolfArr.splice(i, 1);
+				}
+				this.die();
+				this.x = x;
+				this.y = y;
+
+			} else if (matrix[y][x] == 2) {
+				energe_man--;
+
+				matrix[y][x] = 0;
+				matrix[this.y][this.x] = 0;
+
+				for (var i in grassEaterArr) {
+					grassEaterArr.splice(i, 1);
+				}
+				this.die();
+				this.x = x;
+				this.y = y;
+
+			}
+		}
+		return found;
+	}
+
+	die() {
+		matrix[this.y][this.x] = 0;
+		for (var i in ordinary_BulletsArr) {
+			if (this.x == ordinary_BulletsArr[i].x && this.y == ordinary_BulletsArr[i].y) {
+				ordinary_BulletsArr.splice(i, 1);
+				this.multiply = 0;
+			}
+		}
+	}
+
+
+	shoot() {
+
+		var emptyCord_man_move = this.route;
+		this.multiply++;
+
+		var emptyCord = this.chooseCell(emptyCord_man_move);
+		// console.log(shoot_man);
+		// one_click_shoot = true;
+
+		var cord = random(emptyCord);
+		if (cord) {
+			var
+				x = cord[0],
+				y = cord[1];
+			matrix[this.y][this.x] = matrix[y][x];
+			matrix[y][x] = this.index;
+			this.x = x;
+			this.y = y;
+		}
+
+		if (this.multiply == 3) {
+			this.die();
+		}
+	}
+}
